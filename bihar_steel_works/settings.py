86b58da465de50a3f4e7e9a70865334b5a17e879
Ranjan <ranjannp802114@gmail.com>
2025-07-26 14:22:32 +0530

    update setting.py-Allowed_Host

--- bihar_steel_works/settings.py
+++ bihar_steel_works/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-98jq7b&-+5943sw9raxc@8&*+-4pjnk)%tptgn!1y3p3+e_*x7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [biharsteelworks.pythonanywhere.com]
+ALLOWED_HOSTS = ['biharsteelworks.pythonanywhere.com', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
